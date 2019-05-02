cf0ed9753acbdd2673d555d7d10e3bebe30b64d9
camila <guarincamila17@gmail.com>
2019-05-02 09:12:03 -0500

    se organizaorn algunas plantillas, falla con la base de datos

--- usuario/forms.py
+++ usuario/forms.py
@@ -3,6 +3,24 @@ from django.contrib.auth.forms import UserCreationForm
 from usuario.models import Tablero
 from django import forms
 
+class RegistroForm(UserCreationForm):
+
+	class Meta:
+		model = User
+		fields = [
+				'username',
+				'first_name',
+				'last_name',
+				'email',
+			]
+		labels = {
+				'username': 'Nombre de usuario',
+				'first_name': 'Nombre',
+				'last_name': 'Apellidos',
+				'email': 'Correo',
+		}
+
+
 class TableroForm(forms.ModelForm):
 
     class Meta:
